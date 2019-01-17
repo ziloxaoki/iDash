5b03487103de3afd9a4136008fd2cb87b839feec
ziloxaoki <ziloxaoki@gmail.com>
2019-01-17 14:51:56 +1100

    added InputSimulator library

--- ActionSendInput.cs
+++ ActionSendInput.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsInput;
 
 namespace iDash
 {
@@ -24,114 +25,21 @@ namespace iDash
             this.mainForm = mainForm;
         }
 
-        struct INPUT
-        {
-            public int type;
-            public InputUnion u;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        struct InputUnion
-        {
-            [FieldOffset(0)]
-            public MOUSEINPUT mi;
-            [FieldOffset(0)]
-            public KEYBDINPUT ki;
-            [FieldOffset(0)]
-            public HARDWAREINPUT hi;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct MOUSEINPUT
-        {
-            public int dx;
-            public int dy;
-            public uint mouseData;
-            public uint dwFlags;
-            public uint time;
-            public IntPtr dwExtraInfo;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct KEYBDINPUT
-        {
-            /*Virtual Key code.  Must be from 1-254.  If the dwFlags member specifies KEYEVENTF_UNICODE, wVk must be 0.*/
-            public ushort wVk;
-            /*A hardware scan code for the key. If dwFlags specifies KEYEVENTF_UNICODE, wScan specifies a Unicode character which is to be sent to the foreground application.*/
-            public ushort wScan;
-            /*Specifies various aspects of a keystroke.  See the KEYEVENTF_ constants for more information.*/
-            public uint dwFlags;
-            /*The time stamp for the event, in milliseconds. If this parameter is zero, the system will provide its own time stamp.*/
-            public uint time;
-            /*An additional value associated with the keystroke. Use the GetMessageExtraInfo function to obtain this information.*/
-            public IntPtr dwExtraInfo;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct HARDWAREINPUT
-        {
-            public uint uMsg;
-            public ushort wParamL;
-            public ushort wParamH;
-        }
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetMessageExtraInfo();
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
-
-        [DllImport("user32.dll")]
-        static extern int MapVirtualKey(int uCode, uint uMapType);
-
         public override void process(string action, State state)
         {
-            INPUT[] inputs = null;
             ushort keycode = (ushort)Utils.ConvertCharToVirtualKey(action);
-            ushort wscan = (ushort)MapVirtualKey(keycode, 0);
+            
             switch (state)
             {
                 //case State.KeyDown | State.KeyHold:
                 case State.KeyDown:
-                    inputs = getKeyAction(wscan, false);
+                    InputSimulator.SimulateKeyDown((VirtualKeyCode)keycode);
                     break;
 
                 case State.KeyUp:
-                    //inputs = getKeyAction(wscan, false);
-                    //SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
-                    inputs = getKeyAction(wscan, true);
-                    //SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+                    InputSimulator.SimulateKeyUp((VirtualKeyCode)keycode);
                     break;
-            } 
-            
-            if(inputs != null)
-            {
-                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
-            }           
-        }
-
-        private INPUT[] getKeyAction(ushort key, bool isKeyUp)
-        {
-            INPUT[] inputs = new INPUT[]
-                {
-                    new INPUT
-                    {
-                        type = INPUT_KEYBOARD,
-                        u = new InputUnion
-                        {
-                            ki = new KEYBDINPUT
-                            {
-                                wVk = 0, 
-                                wScan = key,
-                                dwFlags = (isKeyUp ? KEYEVENTF_KEYUP : KEYEVENTF_KEYDOWN) | KEYEVENTF_SCANCODE,
-                                //dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_SCANCODE,
-                                dwExtraInfo = GetMessageExtraInfo(),
-                            }
-                        }
-                    }
-                };
-
-            return inputs;
+            }        
         }
     }
 
